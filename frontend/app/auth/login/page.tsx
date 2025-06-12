ac14536dd6e66ef9d9932143ed8096c5f25006ec
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-06-12 10:13:51 +0700

    Enhance login page design with SVG backgrounds and improved layout; update font family in global styles

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -47,34 +47,37 @@ export default function Login() {
     }
 
     return (
-        <div className="bg-cyan-50 h-screen w-full">
-            <div className="max-w-7xl mx-auto relative">
-                <div className="absolute top-12 flex items-center gap-4">
+        <div className="bg-cyan-50 h-screen w-full relative overflow-hidden">
+            <svg viewBox="0 50 200 200" className="absolute -right-42 w-[500px]" xmlns="http://www.w3.org/2000/svg">
+                <path fill="#05B6D3" d="M44,-72C56.5,-69,65.6,-56.1,65.7,-42.4C65.9,-28.8,56.9,-14.4,49.2,-4.5C41.4,5.4,34.9,10.9,32.5,20.6C30.1,30.3,31.9,44.3,27.1,53.7C22.4,63.2,11.2,68.1,-2.1,71.8C-15.5,75.5,-31,78.1,-42.1,72.3C-53.2,66.5,-59.9,52.5,-64,39.1C-68.2,25.6,-69.8,12.8,-62.8,4C-55.8,-4.7,-40.2,-9.5,-35.4,-21.7C-30.5,-33.9,-36.4,-53.5,-32.3,-61.4C-28.2,-69.2,-14.1,-65.3,0.8,-66.8C15.8,-68.2,31.5,-75.1,44,-72Z" transform="translate(100 100)" />
+            </svg>
+            <svg viewBox="0 0 200 200" className="absolute -bottom-[300px] w-[600px]" xmlns="http://www.w3.org/2000/svg">
+                <path fill="#BAE6FF" d="M37,-42.5C44.8,-29.1,45.9,-14.6,44.4,-1.5C42.9,11.6,38.8,23.1,30.9,30.5C23.1,37.9,11.6,41.2,-5.4,46.6C-22.4,52,-44.7,59.5,-54.4,52.1C-64.2,44.7,-61.3,22.4,-59,2.3C-56.7,-17.8,-55.1,-35.6,-45.3,-48.9C-35.6,-62.3,-17.8,-71.2,-1.6,-69.6C14.6,-68,29.1,-55.8,37,-42.5Z" transform="translate(100 100)" />
+            </svg>
+            <div className="w-full flex justify-center relative">
+                <div className="absolute flex items-center top-10">
                     <div className="relative">
-                        <Image src={imgLogin} alt="Image Login" height={600} className="" />
-                        <span className="absolute bg-black opacity-50 inset-0">{''}</span>
-                        <div className="absolute bottom-0 text-white space-y-2">
-                            <h4 className="text-2xl font-semibold">
+                        <Image src={imgLogin} alt="Image Login" height={800} width={800} className="bg-cover rounded-l-lg" />
+                        <span className="absolute bg-black opacity-50 inset-0 rounded-lg">{''}</span>
+                        <div className="absolute bottom-0 text-white space-y-2 p-4">
+                            <h4 className="text-5xl font-semibold leading-[58px]">
                                 Capture Your <br /> Journeys
                             </h4>
-                            <p className="text-sm">Record your travel experience and memories in your personal travel journal.</p>
+                            <p className="text-[15px] text-white leading-6 pr-7 mt-4">Record your travel experience and memories in your personal travel journal.</p>
                         </div>
                     </div>
-                    <div className="">
+                    <div className="w-3/5 h-[500px] p-10 bg-white rounded-r-lg relative">
+                    <h1 className="text-2xl font-semibold mb-7">
+                        Login
+                    </h1>
                         <form className="flex flex-col pt-5 gap-3" onSubmit={handleSubmit}>
-                            <div className="flex w-full items-center border border-[#2E8B57] rounded relative">
-                                <div className="p-3 absolute text-[#8390A2]">
-                                    <MdOutlineEmail className="text-xl" />
-                                </div>
-                                <input type="email" placeholder="Email" className="pl-10 text-[#8390A2] border border-[#2E8B57] focus:outline-none focus:ring-[#2E8B57] rounded px-3 py-2 text-sm w-full"
+                            <div className="flex w-full items-center rounded relative bg-cyan-600/5">
+                                <input type="email" placeholder="Email" className="pl-5 text-[#393e46] focus:outline-none py-3 text-sm w-full"
                                     onChange={e => setEmail(e.target.value)} id={`email`} />
                             </div>
 
-                            <div className="flex w-full items-center border border-[#2E8B57] rounded relative">
-                                <div className="p-3 absolute text-[#8390A2]">
-                                    <GiPadlock className="text-xl" />
-                                </div>
-                                <input type={showPassword ? `text` : `password`} className="pl-10 text-[#8390A2] border border-[#2E8B57] rounded px-3 py-2 text-sm w-full focus:outline-none focus:ring-[#2E8B57]" value={password}
+                            <div className="flex w-full items-center bg-cyan-600/5 rounded relative">
+                                <input type={showPassword ? `text` : `password`} className="pl-5 text-[#393e46] rounded py-3 text-sm w-full focus:outline-none" value={password}
                                     onChange={e => setPassword(e.target.value)} placeholder="Password" id={`password`} />
                                 <div className="cursor-pointer rounded-r-md p-3 absolute right-0" onClick={() => setShowPassword(!showPassword)}>
                                     {showPassword ?
@@ -85,9 +88,13 @@ export default function Login() {
                             </div>
 
                             <button
-                                type="submit" className="mt-3 bg-[#2E8B57] text-white py-2 rounded font-bold cursor-pointer">
-                                Sign In
+                                type="submit" className="mt-3 text-sm font-medium bg-cyan-500 shadow-lg shadow-cyan-200/50 p-[10px] my-1 hover:bg-cyan-100 hover:text-primary  text-white py-2 rounded-full cursor-pointer">
+                                LOGIN
                             </button>
+                            <div className="flex flex-col justify-center text-center">
+                            <p className="text-xs text-slate-500 my-4">Or</p>
+                            <button onClick={() => {router.push("/auth/signup")}} className="uppercase bg-cyan-50 text-primary shadow-cyan-100 border border-cyan-100 hover:bg-primary hover:text-white rounded-full py-2">create account</button>
+                            </div>
                         </form>
                     </div>
                 </div>
